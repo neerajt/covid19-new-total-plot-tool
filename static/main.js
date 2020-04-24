aff1b008d336fea6072c6a9b7541a33a5752e576
jchacko11 <jonathan.j.chacko@gmail.com>
2020-04-23 19:23:26 -0500

    Update chart on API load

--- static/main.js
+++ static/main.js
@@ -1,20 +1,31 @@
-var apiData
+var datasets = []
 
 $( document ).ready(function() {
     console.log( "ready!" );
 	
-	var requestValue = "http://127.0.0.1:8000/plot-data?location_list=Texas:Harris&location_list=New%20York&location_list=New%20York:New%20York%20City";
+	var requestValue = "http://127.0.0.1:8000/plot-data?location_list=Texas:Harris";
 	 $.getJSON(requestValue, function(result){
-        apiData = result
-		}
-	 );
+        var apiData = result.map(makePoint)
+        datasets.push(makeDataset("Testing", apiData))
+         config.data.datasets = datasets
+         myChart.update()
+	 });
 });
 
 function makePoint(point){
     var dateX = new Date(point.x)
-    return {x:dateX, y:point.y}};
+    return {x:dateX, y:point.y}
+}
 
-var plotData = apiData.map(makePoint)
+function makeDataset(name, data) {
+    return {
+        label: name,
+        data: data,
+        fill: false,
+    }
+}
+
+//var plotData = apiData.map(makePoint)
 
 var ctx = document.getElementById("myChart").getContext('2d');
 
@@ -28,16 +39,18 @@ var options = {responsive: true, // Instruct chart js to respond nicely.
     maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
 };
 
-// End Defining data
-var myChart = new Chart(ctx, {
+var config = {
     type: 'scatter',
     data: {
         datasets: [{
                 label: 'Population', // Name the series
                 data: data, // Specify the data values array
-          borderColor: '#2196f3', // Add custom color border            
+          borderColor: '#2196f3', // Add custom color border
           backgroundColor: '#2196f3', // Add custom color background (Points and Fill)
             }]
     },
     options: options
-});
+}
+
+// End Defining data
+var myChart = new Chart(ctx, config);
