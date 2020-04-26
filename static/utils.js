c453cca4df45f93d73564f208003ec460ec9d06b
jchacko11 <jonathan.j.chacko@gmail.com>
2020-04-26 14:21:00 -0500

    Add utils js file

---
+++ static/utils.js
@@ -0,0 +1,47 @@
+//Chart Configuration data
+var options = {
+    scales: {
+        xAxes: [{
+            type: 'time',
+            position: 'bottom',
+        }],
+        yAxes: [{
+            type: 'linear',
+            position: 'bottom'
+        }]
+    },
+    responsive: true, // Instruct chart js to respond nicely.
+    maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height
+};
+
+var config = {
+    type: 'scatter',
+    data: {datasets: datasets},
+    options: options
+}
+
+//end config data
+
+
+function makePoint(point) {
+    var dateX = new Date(point.x)
+    return {x: dateX, y: point.y}
+}
+
+function makeDataset(name, data) {
+    return {
+        label: name,
+        data: data,
+        fill: false,
+    }
+}
+
+function initChart() {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    myChart = new Chart(ctx, config);
+}
+
+function updateChart(datasets) {
+    config.data.datasets = datasets
+    myChart.update()
+}
