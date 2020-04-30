8cd9cc11f830750ade7e0175d61e9072a6257d8a
Neeraj Tandon <neerajtandon25@gmail.com>
2020-04-29 19:10:44 -0500

    add logic for different environments to api.R

--- api.R
+++ api.R
@@ -3,10 +3,13 @@ pwd <- Sys.getenv("PWD")
 
 print(paste("pwd var is set to", pwd))
 
-if(pwd != ""){
+if(pwd == "/"){
+    app_path <- "/app/plot_function.R"
+} else if(pwd != ""){
     app_path <- paste(pwd, "plot_function.R", sep="/")
 } else{
     app_path <- "plot_function.R"
 }
+
 r <- plumb(app_path)  # Where 'plumber.R' is the location of the file shown above
 r$run(port=8000)
