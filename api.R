6046f6782a5c5d94b67f06945f1ab5eb4969a3b6
Neeraj Tandon <neerajtandon25@gmail.com>
2020-04-29 19:19:25 -0500

    add heroku port to api.R

--- api.R
+++ api.R
@@ -1,5 +1,6 @@
 library(plumber)
 pwd <- Sys.getenv("PWD")
+heroku_port <- Sys.getenv("PORT")
 
 print(paste("pwd var is set to", pwd))
 
@@ -11,5 +12,11 @@ if(pwd == "/"){
     app_path <- "plot_function.R"
 }
 
+if(heroku_port == ""){
+    port <- 8000
+} else if(heroku_port != ""){
+    port <- heroku_port
+}
+
 r <- plumb(app_path)  # Where 'plumber.R' is the location of the file shown above
-r$run(port=8000)
+r$run(port=port)
